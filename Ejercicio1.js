63d8641abfed67865bb5989c08bad6c88b5654a8
Mirel <mirelpisica@gmail.com>
2022-06-01 17:09:37 +0200

    Ejercicio1

---
+++ Ejercicio1.js
@@ -0,0 +1,12 @@
+// Un nuevo Set con los nombres de tu familia
+
+const array = ("Mirel", "Paco", "Tamara");
+const miSet = new Set(array)
+
+//Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+
+miSet.add("Mirel")
+
+// Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+
+miSet.add("JaveScript")
